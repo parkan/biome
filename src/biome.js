7be9967c895640d568b7fa7c49a3c101806d6e53
Arkadiy Kukarkin <arkadiy@mindhole.org>
2018-07-29 17:44:04 -0700

    Use (and require) local rdv server

--- src/biome.js
+++ src/biome.js
@@ -12,7 +12,8 @@ const defaults = {
 }
 const seedPeers = [
     //'/dns4/wrtc-star.discovery.libp2p.io/tcp/443/wss/p2p-webrtc-star',
-    '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+    '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
+    '/ip4/127.0.0.1/tcp/9090/ws/p2p-websocket-star'
 ]
 
 module.exports = (options) => {
