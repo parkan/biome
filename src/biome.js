239ca59303e747db9b79924fab3a3f13ff811608
Arkadiy Kukarkin <arkadiy@mindhole.org>
2018-08-01 09:51:47 -0700

    Fix signaling servers

--- src/biome.js
+++ src/biome.js
@@ -10,14 +10,15 @@ const defaults = {
     ],
     swarm: [
         '/dns4/ws-star2.sjc.dwebops.pub/tcp/443/wss/p2p-websocket-star',
-        '/dns4/relay.decentralizedweb.net/tcp/9090'
+        '/dns4/relay.decentralizedweb.net/tcp/9090/ws/p2p-websocket-star',
+        '/dns4/ws-star1.par.dwebops.pub/tcp/9090/ws/p2p-websocket-star/'
     ],
     relay: {
         relayWSAddr: '/dns4/relay.decentralizedweb.net/tcp/4004/wss/ipfs/QmPdHHgEr1gKbMuhiBf6545BL7mxaKbmCKbaJE7yY4CkBg',
         apiAddr: '/dns4/relay.decentralizedweb.net/tcp/5004/'
     },
     keys: '4XTTMA1FxhTNufWa7LmW5MvMw2zEgUWP7G5SuwzU4epmRmPam-K3TgUUKyYR7sbt61ej8jnhdbQVLUaGsawW1QHs2nzFpoXVcNaMiyXictHKPz1NQPeRgbDcqqLroatJbwkMeo3kHnUqQtyGZGfgxqXUF3y5Wm3fPkTiRs2ftakJWjRF7ZpLq7Mnfo', // TODO: replace w/RO key by default
-    nonce: '11112' // change to abandon previous log
+    nonce: '11113' // change to abandon previous log
 }
 
 module.exports = (options) => {
